6b8a64ec1a661a532b4c280d61915f36e4b0832c
Stephen W <43740055+0x737761726e73@users.noreply.github.com>
2019-07-02 06:35:23 +0000

    Add chocolateyinstall.ps1

---
+++ ungoogled-chromium-64/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/Wyse-/ungoogled-chromium-binaries/releases/download/67.0.3396.87-3/ungoogled-chromium_67.0.3396.87-3_windows.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url64bit      = $url64
+
+  softwareName  = 'ungoogled-chromium-64*'
+
+  checksum64    = 'bab1f622f9a966baffa07838c305e586715979db884cd7369df96bb08c092f8a'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Remove-Item $toolsDir\*.zip -ea 0
