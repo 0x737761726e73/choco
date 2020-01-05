978c205b76472478dfca5904022e246271860720
0x737761726e73 <swarns@tutanota.com>
2020-01-05 12:40:01 +0000

    Update for v0.70.122

--- dissenter-browser/tools/chocolateyinstall.ps1
+++ dissenter-browser/tools/chocolateyinstall.ps1
@@ -1,19 +1,17 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://dissenter.com/dist/browser/0.66.99/dissenter-browser-v0.66.99.exe'
- 
+$url64      = 'https://github.com/gab-ai-inc/defiant-browser/releases/download/v77.120/dissenter_installer_78_0_70_122.exe'
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
   url64bit      = $url64
- 
-  softwareName  = 'dissenter-browser*'
- 
-  checksum64    = '1d031b6bbe98d33b6d8487ff8dc981a1'
+
+  checksum64    = 'df77d2e79f29be959b2f4f3c20684513'
   checksumType64= 'md5'
- 
+
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
- 
+
 Install-ChocolateyPackage @packageArgs
